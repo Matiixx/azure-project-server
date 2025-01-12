bc3657d71af3c2a9442574a553ff2bd1c84d49a4
Matiixx <87618425+Matiixx@users.noreply.github.com>
2025-01-12 15:57:03 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import sql from "mssql";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+const config: sql.config = {
+  user: "nimda",
+  password: process.env.SERVER_ADMIN_PASSWORD,
+  server: "agonista-receptorow-dopaminergicznych.database.windows.net",
+  database: "anhedonia",
+  port: 1433,
+  options: {
+    encrypt: true,
+    trustServerCertificate: false,
+    connectTimeout: 30000,
+  },
+};
+
+app.listen(port, async () => {
+  console.log(`Server running on port ${port}`);
+  let connection: sql.ConnectionPool | undefined;
+  try {
+    connection = await sql.connect(config);
+    const result = await sql.query`SELECT TOP 10 * FROM Ratings`;
+    console.log(result);
+    await connection.close();
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await connection?.close();
+  }
+});
