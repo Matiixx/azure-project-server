b90df82b9c59cc7e2690082e19365b47bcf9f663
Matiixx <87618425+Matiixx@users.noreply.github.com>
2025-01-13 18:28:31 +0100

    other chat changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+import express from 'express'
 
 const app = express()
 const port = 3000
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+export default app
